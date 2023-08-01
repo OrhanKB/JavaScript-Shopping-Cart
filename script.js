26fa5a64e89b01b8e313ea784d633eb0ae665a79
Orhan <kurtboganorhan@gmail.com>
2023-08-02 00:55:51 +0300

    nav-buttons completed / see more button completed / css add to card added / header

--- script.js
+++ script.js
@@ -4,6 +4,12 @@ const usersAPI = "https://fakestoreapi.com/users"
 
 //DOC
 const items = document.querySelector(".items")
+const menBtn = document.querySelector("#men")
+const womenBtn = document.querySelector("#women")
+const jewBtn = document.querySelector("#jewelery")
+const ourProducts = document.querySelector(".our-products")
+const seeMoreBtn = document.querySelector(".mid-btn")
+const body = document.body
 //DOC
 
 
@@ -49,19 +55,69 @@ const fetchProducts = async () => {
             //PRICE
 
             // BUY BUTTON
-            const buy = document.createElement("button")
+             const buy = document.createElement("button")
             const bişey = document.createElement("img")
             bişey.src = "img/shopping-cart.png"
 
             buy.classList.add("add-cart")
             buy.innerHTML = "Add To Cart"
-            divElement.appendChild(buy)
+            divElement.appendChild(buy) 
+
+            const buyImg = document.createElement("img");
+            buyImg.src = "img/shopping-cart.png";
+            buyImg.alt = "Buy";
+            buyImg.classList.add("buy-img")
+            buy.appendChild(buyImg);
             // BUY BUTTON
 
-            
+            //FILTERING(men)
+            menBtn.addEventListener("click", e => {
+                if(product.category !== "men's clothing" ) {
+                    divElement.style.display = "none"
+                } else{
+                divElement.style.display ="flex"
+                ourProducts.innerHTML = "MEN'S CLOTHING"
+                window,scrollBy(0, 600)
+                }
+            })
+            //FILTERING(men)
+
+            //FILTERING(women)
+            womenBtn.addEventListener("click", e => {
+                if(product.category !== "women's clothing") {
+                    divElement.style.display = "none"
+                } else {
+                    divElement.style.display = "flex"
+                    ourProducts.innerHTML = "WOMEN'S CLOTHING"
+                    window,scrollBy(0, 600)
+                }
+            })
+            //FILTERING(women)
+
+            //FILTERING(jewelery)
+            jewBtn.addEventListener("click", e => {
+                if(product.category !== "jewelery") {
+                    divElement.style.display = "none"
+                } else {
+                    divElement.style.display = "flex"
+                    ourProducts.innerHTML  = "JEWELERY"
+                    window,scrollBy(0, 600)       
+                }
+            })
+            //FILTERING(jewelery)
+
+            //MID-BTN SCROLL
+           seeMoreBtn.addEventListener("click", e => {
+            window,scrollBy(0, 550)
+           })
+            //MID-BTN SCROLL
         }
          });
         
+
+         if(body.style.height < "1000px") {
+            body.style.height = "1200px"
+         }
 
     } catch(err) {
         console.log(err)
@@ -69,19 +125,6 @@ const fetchProducts = async () => {
 }
 
 fetchProducts()
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 const fetchUsers = async () => {
